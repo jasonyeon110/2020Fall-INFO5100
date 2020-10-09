7eb309bfc95ca57a97e702b8919aa87fe1b5c92f
jasonyeon110 <jasonyeon110@gmail.com>
2020-10-08 22:11:44 -0700

    Assignment_4 Uploaded v2

--- Info5100/src/Assignment4/LargestElement.java
+++ Info5100/src/Assignment4/LargestElement.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LargestElement {
+	
+	
 
 	//find a duplicated number and put out the unique elements from the array.
 	public static ArrayList<Integer> removeDups(int[] arr) {
 		
-		ArrayList <Integer> arr_new = new ArrayList<>();
+		ArrayList <Integer> arr_new = new ArrayList<>();	
 		
 		int i = 0;
 			while (i < arr.length) {
@@ -27,27 +29,38 @@ public class LargestElement {
 				}
 	
 			}
-			System.out.println("Printing out Dup removed array");
+			//System.out.println("Printing out Dup removed array");
 			System.out.println(arr_new);
 			return arr_new;
 		}
 	
+	//Actual code for the first problem
+	public static int kthElement(int k, int[] arr) {
+		int[] a = arr;
+		Arrays.sort(a);
+		//System.out.println(a);
+		int b = a.length;
+		int i = b-k;
+		System.out.println(k + "th Element in the Array is " + arr[i]);
+		return arr[i];
+	}
 
-	//sort an array in numeric order and find the kth element in the array
-	public static int kthElement(int k, int[] array) {
-		int[] a = array;
+	/*sort an array in numeric order and find the kth element in the array
+	 * So first I misunderstood the first problem. I thought we had to remove duplicated numbers and
+	 * look for the kth's element and this is how I did it.
+	 */
+	public static int kthElementSortDupRemoved(int k, int[] arr) {
+		int[] a = arr;
 		Arrays.sort(a);
 		int b = removeDups(a).size();
 		int i = removeDups(a).get(b-k);
 		
 		
+		System.out.println(k + "th Largest element in this Sorted & Removed duplicated array is " + i);
+		
 		return i;
 	}
-	
-	public static void main(String[] args) {
-		int[] b = {1,4,5,6,7,3,2,4,6,7,8,9,9,8,7,6,5,4,3};
-		System.out.println(kthElement(1,b));
-		//removeDups(b);		
 
-	}
+ 
+	
 }
